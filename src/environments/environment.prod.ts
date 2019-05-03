81e794b225c665eb87b68c0a8e049a430e07e630
Dear Roy <royatrangpur@gmail.com>
2019-05-03 23:47:47 +0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyBPirg61xfvdKI3IR0SJ2W_pt-cC4Y4odE",
-    authDomain: "projecttwo-e6bb0.firebaseapp.com",
-    databaseURL: "https://projecttwo-e6bb0.firebaseio.com",
-    projectId: "projecttwo-e6bb0",
-    storageBucket: "projecttwo-e6bb0.appspot.com",
-    messagingSenderId: "69888985569",
-    appId: "1:69888985569:web:683e0341b681d804"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
